f226f6b874f03aeddb38ed3153184f2947099c0f
og3da <rapha488@gmail.com>
2022-01-18 14:18:26 -0300

    seção 2 - Python Básico

--- secao2/desafios/desafio-validarCPF.py
+++ secao2/desafios/desafio-validarCPF.py
@@ -12,14 +12,6 @@ resultado = 11 - (resultado % 11)
 se for > que 9, digito sera = 0
 acima conseguimos o segundo digito do cpf
 """
-""" from operator import mul
-def calculoDigito(i):
-    digito = 11 - (i % 11)
-    if digito>9:
-        digito=0
-    else:
-        digito=digito
-    print(f'valor do digito: {digito}')  """
 
 cpf=input("Digite seu CPF: ")
 cpf_temp=[]
@@ -27,22 +19,48 @@ cpf_temp=[]
 for valor in cpf:
     cpf_temp+=valor
 
-# aqui estou multiplicando cada valor pelo multiplicador e concatenando os resultados para chegar ao 1º digito
-multiplicador=11
+# aqui estou multiplicando cada valor pelo multiplicador e concatenando os resultados (soma+= int(i) *multiplicador) para chegar ao 1º digito
+soma=0
+multiplicador=10
 for i in cpf_temp:
-    i=int(i)
-    i = (i*multiplicador)
+    soma+= int(i) *multiplicador
     multiplicador-=1
-    print(f'valor de i: {i},valor de multiplicador {multiplicador}')
-    if multiplicador<=2:
+    if multiplicador==1:
         break
     else:
         continue
+
 print("")
-i=int(i)
-digito1 = 11 - (i % 11)
+digito1 = 11 - (soma % 11)
 if digito1>9:
     digito1=0
 else:
     digito1=digito1
-print(f'valor do digito: {digito1}')  
+print(f'valor do digito 1: {digito1}')  
+
+#  para chegar ao 2º digito
+soma=0
+multiplicador=11
+for i in cpf_temp:
+    soma+= int(i) *multiplicador
+    multiplicador-=1
+    if multiplicador==1:
+        break
+    else:
+        continue
+
+digito2 = 11 - (soma % 11)
+if digito2>9:
+    digito2=0
+else:
+    digito2=digito2
+print(f'valor do digito 2: {digito2}')  
+
+# validando o cpf
+print("")
+if (digito1==int(cpf_temp[9])) and (digito2==int(cpf_temp[10])):
+    print("CPF VÁLIDO!")
+    print("--- created by og3da ---")
+else:
+    print("CPF INVÁLIDO!")
+    print("--- created by og3da ---")
