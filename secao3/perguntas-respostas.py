9620406a07f01968d504663470bd054c08b3d420
og3da <rapha488@gmail.com>
2022-01-23 16:38:20 -0300

    jogo de perguntas e respostas com dicionario em Python

---
+++ secao3/perguntas-respostas.py
@@ -0,0 +1,36 @@
+"""
+SISTEMA DE PERGUNTAS E RESPOSTAS COM DICIONARIO
+"""
+perguntas = {
+    'pergunta 1': {
+        'pergunta' : 'quanto é 2+2? ',
+        'respostas' : {'a' : 4, 'b' : 8, 'c' : 10,},
+        'resposta certa':'a',
+    },
+    'pergunta 2': {
+        'pergunta' : 'quanto é 2+20? ',
+        'respostas' : {'a' : 20, 'b' : 22, 'c' : 202,},
+        'resposta certa':'b',
+    },
+    'pergunta 3': {
+        'pergunta' : 'quanto é 2*2? ',
+        'respostas' : {'a' : 4, 'b' : 8, 'c' : 10,},
+        'resposta certa':'a',
+    },
+}
+
+acertos=0
+for pk, pv in perguntas.items():
+    print(f'{pk}: {pv["pergunta"]}')
+    print("Escolha uma das opções abaixo: ")
+    for rk, rv in pv['respostas'].items():
+        print(f'{rk}: {rv}')
+
+    resposta_user=input("Digite sua resposta: ")
+    if resposta_user == pv['resposta certa']: 
+        print("Parabens vc acertou!")
+        acertos+=1
+    else:
+        print("Você errou")
+    print()
+print(f'acertos= {acertos}')
