6c97e8378a816097b4771096b917521f165f8b88
og3da <rapha488@gmail.com>
2022-02-02 16:52:13 -0300

    levantando exceções com raise, corrigido

--- secao3/levantando-excecoes.py
+++ secao3/levantando-excecoes.py
@@ -1,11 +1,15 @@
 """
 LEVANTANDO EXCEÇÕES USANDO 'RAISE' COM PYTHON
 - usando raise ValueError("mensagem") podemos criar nossa propria exceção, passando a mensagem desejada
+- no caso raise vai fazer com que a mensagem padrao de resposta da exceção seja a que for definida na função
 """
 def dividir(n1,n2):
     if n2 == 0:
-        raise ValueError("Digite um numero diferente de 0!")
+        raise ValueError("Digite um numero diferente de 0!") # essa mensagem sera retornada ao inves da msg padrao de erro 
     else:
         return n1 / n2
 
-print(dividir(1,0))
+try:
+    print(dividir(1,0))
+except ValueError as error:
+    print(error) # aqui estamos printando a mensagem de erro
